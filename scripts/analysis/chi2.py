e800129e6f7c5ffbda039ddc93f6c0d8737c4fe7
Gray Putnam <gputnam@uboonegpvm05.fnal.gov>
2017-12-04 09:41:04 -0600

    fixed previous oversight on merge

--- scripts/analysis/chi2.py
+++ scripts/analysis/chi2.py
@@ -345,13 +345,7 @@ def plot_spectra(emx, em, eex, ee, ess, fcov, pot=5e19, eff_m=0.3, eff_e=0.3):
     ax2.xaxis.set_major_locator(ticker.MultipleLocator(500))
     ax2.set_ylim(0, 1.1 * np.max(ee+es))
     ax2.legend(loc='upper right')
-<<<<<<< HEAD
-    
     plt.tight_layout()
-=======
-   
->>>>>>> don't run mcmc's for now
-    plt.show()
 
 
 def plot_sigma_v_eff(em, ee, es, fcov, pot=5e19, mc=False):
@@ -526,19 +520,11 @@ if __name__ == '__main__':
 
         print('6.6e20 stat mc: %f' %
                significance(em, ee, es, fcov,
-<<<<<<< HEAD
-               pot=66e19, syst=False, mc=False, eff_m=0.3, eff_e=0.3))
-
-        print('6.6e20 syst mc: %f' %
-               significance(em, ee, es, fcov,
-               pot=66e19, syst=True, mc=False, eff_m=0.3, eff_e=0.3))
-=======
                pot=66e19, syst=False, mc=args.mcmc, eff_m=0.3, eff_e=0.3))
 
         print('6.6e20 syst mc: %f' %
                significance(em, ee, es, fcov,
                pot=66e19, syst=True, mc=args.mcmc, eff_m=0.3, eff_e=0.3))
->>>>>>> added argparse for mcmc on chi2.py
 
         #print('5.0e19 syst mc: %f' %
         #       significance(em, ee, es, fcov,
