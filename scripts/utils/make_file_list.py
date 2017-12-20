9c247db89ed42c38529581a6bfd3e6a74e60d35f
Gray Putnam <gputnam@uboonegpvm02.fnal.gov>
2017-12-20 16:17:52 -0600

    now test if root files are corrupted when generating file list

--- scripts/utils/make_file_list.py
+++ scripts/utils/make_file_list.py
@@ -1,9 +1,13 @@
 import glob
 import sys
+import ROOT
 
 file_list = glob.glob(sys.argv[1])
 f_out = sys.argv[2]
 with open(f_out, "w+") as f:
     for f_name in file_list:
-        f.write("%s\n" % f_name)
+        if not ROOT.TFile(f_name).IsZombie():
+            f.write("%s\n" % f_name)
+        else:
+            print "ZOMBIE: %s" % f_name
 
